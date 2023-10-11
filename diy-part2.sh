d1d0869644f6816557ae2b99f0bb68f2c46899d3
sir-this-way <146545517+sir-this-way@users.noreply.github.com>
2023-10-11 19:29:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,9 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.7.1/192.168.7.99/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.7.1/192.168.7.101/g' package/base-files/files/bin/config_generate
 
 # Modify default IP
 #!/bin/bash
 #=================================================
-sed -i "s/192.168.7.1/192.168.7.99/" package/base-files/files/bin/config_generate
+sed -i "s/192.168.7.1/192.168.7.101/" package/base-files/files/bin/config_generate
